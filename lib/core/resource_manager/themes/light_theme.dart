dc13b9ab081e1504461ea4faf965cecfa98e346e
Hussein Yasser <119596187+Hussein2077@users.noreply.github.com>
2024-01-23 10:44:26 +0200

    .

--- lib/core/resource_manager/themes/light_theme.dart
+++ lib/core/resource_manager/themes/light_theme.dart
@@ -1,74 +1,47 @@
-
 import 'package:flutter/material.dart';
-
-
-
-
 
 //todo pass var here
 ThemeData lightTheme = ThemeData(
-    textTheme:  TextTheme(
-    headlineLarge: const TextStyle(
-      fontFamily: 'GothamMedium',
-      fontSize:20,
-      color: Colors.black,
-      overflow: TextOverflow.ellipsis
+    textTheme: TextTheme(
+      headlineLarge: const TextStyle(
+          fontFamily: 'GothamMedium',
+          fontSize: 20,
+          color: Colors.black,
+          overflow: TextOverflow.ellipsis),
+      headlineMedium: const TextStyle(
+          fontFamily: 'GothamMedium',
+          fontSize: 18,
+          color: Colors.black,
+          overflow: TextOverflow.ellipsis),
+      bodyLarge: const TextStyle(
+          fontFamily: 'GothamMedium',
+          fontSize: 16,
+          color: Colors.black,
+          overflow: TextOverflow.ellipsis),
+      bodyMedium: const TextStyle(
+          fontFamily: 'GothamMedium',
+          fontSize: 14,
+          color: Colors.black,
+          overflow: TextOverflow.ellipsis),
+      bodySmall: const TextStyle(
+          fontFamily: 'GothamMedium',
+          fontSize: 12,
+          color: Colors.black,
+          overflow: TextOverflow.ellipsis),
+      titleMedium: TextStyle(
+          fontFamily: 'GothamMedium',
+          fontSize: 16,
+          color: Colors.black.withOpacity(0.4),
+          overflow: TextOverflow.ellipsis),
+      titleSmall: TextStyle(
+          fontFamily: 'GothamMedium',
+          fontSize: 12,
+          color: Colors.black.withOpacity(0.4),
+          overflow: TextOverflow.ellipsis),
     ),
-    headlineMedium:  const TextStyle(
-      fontFamily: 'GothamMedium',
-      fontSize: 18,
-      color: Colors.black,
-            overflow: TextOverflow.ellipsis
-
-    ),
-    
-    bodyLarge: const TextStyle(
-      fontFamily: 'GothamMedium',
-      fontSize:16,
-      color: Colors.black,
-            overflow: TextOverflow.ellipsis
-
-    ),
-     bodyMedium:const TextStyle(
-      fontFamily: 'GothamMedium',
-      fontSize: 14,
-      color: Colors.black,
-            overflow: TextOverflow.ellipsis
-
-    ), 
-      bodySmall:const TextStyle(
-      fontFamily: 'GothamMedium',
-      fontSize: 12,
-      color: Colors.black,
-            overflow: TextOverflow.ellipsis
-
-    ), 
-
-titleMedium: TextStyle(
-      fontFamily: 'GothamMedium',
-      fontSize: 16,
-      color: Colors.black.withOpacity(0.4),
-            overflow: TextOverflow.ellipsis
-
-    ), 
- titleSmall: TextStyle(
-      fontFamily: 'GothamMedium',
-      fontSize: 12,
-      color: Colors.black.withOpacity(0.4),
-            overflow: TextOverflow.ellipsis
-
-    ), 
-   
-  ),
-  fontFamily: 'GothamMedium',
-  brightness: Brightness.light ,
-  colorScheme:  const ColorScheme.light(
-    background: Colors.white , 
-    primary: Colors.black,
-
-
-  )
-);
-
-
-
+    fontFamily: 'GothamMedium',
+    brightness: Brightness.light,
+    colorScheme: const ColorScheme.light(
+      background: Colors.white,
+      primary: Colors.black,
+    ));
